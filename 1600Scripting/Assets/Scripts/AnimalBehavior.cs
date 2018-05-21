4ab71a490e74d14d4a6a79f4b8406bc508d51bc6
Chase Hacking <chase.hacking@gmail.com>
2018-05-21 11:17:47 -0600

    Dog/Horse Objects created with info/health

--- 1600Scripting/Assets/Scripts/AnimalBehavior.cs
+++ 1600Scripting/Assets/Scripts/AnimalBehavior.cs
@@ -9,9 +9,10 @@ public class AnimalBehavior : MonoBehaviour {
 	public Animal AnimalInfo;
 	// Use this for initialization
 	void Start () {
-		
+		print (AnimalInfo.Health);
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
 		
