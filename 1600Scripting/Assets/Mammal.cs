bea16063f76ae4585b7fff8c91ff4901444e41cb
Chase Hacking <chase.hacking@gmail.com>
2018-05-21 11:04:34 -0600

    Animal Colors added to Dog/Horse Mammals

--- 1600Scripting/Assets/Mammal.cs
+++ 1600Scripting/Assets/Mammal.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class Mammal : Animal {
+
+
+	public int HairCount = 1000;
+	public Color HairColor = Color.black;
 
 
 }
