d201458c081132b0b39dacea8558ce94518bb7dc
Chase Hacking <chase.hacking@gmail.com>
2018-05-21 10:51:52 -0600

    Animal attributes, added Asset Menu to script

--- 1600Scripting/Assets/Scripts/Animal.cs
+++ 1600Scripting/Assets/Scripts/Animal.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class Animal : ScriptableObject {
 
 	public float Health = 1.0f;
-
-
-
-
-
-
-
-
-
+	public float Speed = 1.0f;
+	public float Strength = 1.0f;
+	public int LegCount = 2;
 
 }
