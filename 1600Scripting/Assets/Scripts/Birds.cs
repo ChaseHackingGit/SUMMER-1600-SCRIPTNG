06419e9068684a4e809d9691fb229305e203c7ba
Chase Hacking <chase.hacking@gmail.com>
2018-05-25 10:10:47 -0600

    Finished basic Birds Script

--- 1600Scripting/Assets/Scripts/Birds.cs
+++ 1600Scripting/Assets/Scripts/Birds.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class Birds : Animal {
+
+
+	public int FeatherCount = 1000;
+	public Color Feather = Color.blue;
+	public int FlightSpeedCount = 10;
 
 
 }
