ffa520b52ed0230b2f32cc4153c1d92b0d72e9da
Chase Hacking <chase.hacking@gmail.com>
2018-06-11 10:38:50 -0600

    catching up on missed work by adding new scripts to change color of objects/edit behaviors/etc.

---
+++ 1600Scripting/Assets/Scripts/ChangeUIColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ChangeUIColor : ScriptableObject
+{
+	public Color NewColor;
+
+	public void ColorUI(Image imageParameter)
+	{
+		imageParameter.color = NewColor;
+	}
+}
