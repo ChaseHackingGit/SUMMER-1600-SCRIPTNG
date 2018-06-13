da26afcbeedff602c3801ec89ddcab0d8e782c0b
Chase Hacking <chase.hacking@gmail.com>
2018-06-13 10:32:40 -0600

    edited spacing on Move Player Script

--- 1600Scripting/Assets/Scripts/MovePlayer.cs
+++ 1600Scripting/Assets/Scripts/MovePlayer.cs
@@ -8,20 +8,22 @@ public class MovePlayer : MonoBehaviour {
 
 	private Vector3 newPosition;
 	public float Speed = 10.0f;
-	public float Gravity = 9.82f;
+	public float Gravity = 9.81f;
 	public float JumpSpeed = 10.0f;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		controller = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
-
-
-		newPosition.y = JumpSpeed;
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			newPosition.y = JumpSpeed;
+		}
 
 		newPosition.y -= Gravity;
 		newPosition.x = Speed * Input.GetAxis ("Horizontal");
