93d01ba0e574d6bf2940c1d672d9d633d9b1d3f6
Chase Hacking <chase.hacking@gmail.com>
2018-06-13 11:17:08 -0600

    True/False If statement code start (ERRORS)

--- 1600Scripting/Assets/Scripts/IfStatements.cs
+++ 1600Scripting/Assets/Scripts/IfStatements.cs
@@ -2,7 +2,17 @@
 
 [CreateAssetMenu]
 public class IfStatements : ScriptableObject {
-	{
+{
+		
+		public int a;
+		public int b;
+		public int c;
 
-
+		private void OnEnable()
+		{
+			if (a + b != c)
+			{
+				Debug.Log("True");
+			}
+		}
 	}
