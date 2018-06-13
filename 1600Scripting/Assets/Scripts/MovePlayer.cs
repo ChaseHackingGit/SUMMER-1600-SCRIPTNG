e403ef509656320c77e95d3192efd6e37d860c52
Chase Hacking <chase.hacking@gmail.com>
2018-06-13 10:54:20 -0600

    new Scene

--- 1600Scripting/Assets/Scripts/MovePlayer.cs
+++ 1600Scripting/Assets/Scripts/MovePlayer.cs
@@ -21,6 +21,8 @@ public class MovePlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+
+
 		if (Input.GetKeyDown (KeyCode.Space)) 
 		{
 			newPosition.y = JumpSpeed;
