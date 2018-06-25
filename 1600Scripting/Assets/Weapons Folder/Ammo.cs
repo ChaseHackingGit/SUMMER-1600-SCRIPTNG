443ab8b9fb240136768ac33d612945e2e720fe8e
Chase Hacking <chase.hacking@gmail.com>
2018-06-25 11:15:32 -0600

    added ammo upgrade to ammo script acting as a power up for ammo

--- 1600Scripting/Assets/Weapons Folder/Ammo.cs
+++ 1600Scripting/Assets/Weapons Folder/Ammo.cs
@@ -10,10 +10,17 @@ public class Ammo : MonoBehaviour {
 
 	private Rigidbody projectile;
 	public FloatData ProjectileSpeed;
+	public FloatData ProjectileUpgrade;
 
 	private void start()
 	{
 		projectile = GetComponent<Rigidbody> ();
+		projectile.AddForce (0,0,ProjectileSpeed.Value);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		ProjectileSpeed = ProjectileUpgrade;
 		projectile.AddForce (0,0,ProjectileSpeed.Value);
 	}
 }
