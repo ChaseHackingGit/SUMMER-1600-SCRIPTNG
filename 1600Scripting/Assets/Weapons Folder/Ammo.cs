8505e12e8d6ddaf107df89e17ff7a56a0cc2bbd0
Chase Hacking <chase.hacking@gmail.com>
2018-06-25 10:51:54 -0600

    ammo class with private rigid body tied with script to give it a basic force/direction

--- 1600Scripting/Assets/Weapons Folder/Ammo.cs
+++ 1600Scripting/Assets/Weapons Folder/Ammo.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
 public class Ammo : MonoBehaviour {
 
 	private Rigidbody projectile;
 
 	private void start()
 	{
-
+		projectile = GetComponent<Rigidbody> ();
+		projectile.AddForce (0,0,100);
 	}
-
-
 }
