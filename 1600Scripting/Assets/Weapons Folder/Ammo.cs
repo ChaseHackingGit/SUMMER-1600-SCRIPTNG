be958159d903f6863e5e1d2e1190529d6a1c2a2d
Chase Hacking <chase.hacking@gmail.com>
2018-06-27 10:38:39 -0600

    editing ammo script, made plans for new Project to fix mysterious errors

--- 1600Scripting/Assets/Weapons Folder/Ammo.cs
+++ 1600Scripting/Assets/Weapons Folder/Ammo.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
-
-
 public class Ammo : MonoBehaviour {
 
 	private Rigidbody Projectile;
@@ -16,6 +13,12 @@ public class Ammo : MonoBehaviour {
 	{
 		Projectile = GetComponent<Rigidbody> ();
 		Projectile.AddForce (0,0,ProjectileSpeed.Value);
+		Invoke ("DestroyObject", 2);
+	}
+
+	private void DestroyObject()
+	{
+		Destroy (gameObject);
 	}
 
 	private void OnTriggerEnter(Collider other)
