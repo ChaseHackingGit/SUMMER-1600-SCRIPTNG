6b9d64cd4f5e3a1203341457ebb4d7ddd4e0f3f2
Chase Hacking <chase.hacking@gmail.com>
2018-07-09 11:06:28 -0600

    AI movement Script created

---
+++ 1600Scripting/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent( typeof(NavMeshAgent ) )]
+
+public class AIMovement : MonoBehaviour 
+{
+	public NavMeshAgent Agent;
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+}
