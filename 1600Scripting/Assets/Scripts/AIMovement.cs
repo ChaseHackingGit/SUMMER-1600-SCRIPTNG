1f8139aed2dedd34648fd6dff30492dec296d1fc
Chase Hacking <chase.hacking@gmail.com>
2018-07-09 11:21:42 -0600

    removed loop script to remove compiler (temp.) AI follows player to destination with script

--- 1600Scripting/Assets/Scripts/AIMovement.cs
+++ 1600Scripting/Assets/Scripts/AIMovement.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-
 [RequireComponent( typeof(NavMeshAgent ) )]
 
 public class AIMovement : MonoBehaviour 
 {
 	public NavMeshAgent Agent;
+	public Transform Destination;
 
 	private void Start()
 	{
-
+		Agent = GetComponent<NavMeshAgent> ();
 	}
 
 	private void Update()
 	{
-
+		Agent.destination = Destination.position;
 	}
 }
