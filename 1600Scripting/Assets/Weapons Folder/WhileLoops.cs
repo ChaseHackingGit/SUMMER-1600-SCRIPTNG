1b9f8e19096884fd9a270919701162b477469439
Chase Hacking <chase.hacking@gmail.com>
2018-07-09 10:59:20 -0600

    new loop script started, new scene (AI) started, begin makeup for Moon Lake

---
+++ 1600Scripting/Assets/Weapons Folder/WhileLoops.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour 
+{
+
+	public Text DisplayText;
+
+	public int i = 3;
+
+	//Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (Ticker ());
+	}
+	IEnumerator Ticker()
+	{
+		while (i > 0)
+		{
+			DisplayText.text = i.ToString();
+
+			yield return new WaitForSeconds(1);
+			i--;
+		}
+		DisplayText.text = "GO!";
+
+		yield return new WaitForSeconds(2);
+
+		DisplayText.text = "";
+	}
+}
+				
