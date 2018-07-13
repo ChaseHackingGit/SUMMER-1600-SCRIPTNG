1624b1df90b6184d9a2d5554275d4aa5a51788ff
Chase Hacking <chase.hacking@gmail.com>
2018-07-13 16:14:11 -0600

    made a loop in WhileLoops but edited it so prevent crashing (WaitForSeconds(1))

--- 1600Scripting/Assets/SCENES (REDONE STUDY)/Scripts (Redone Practice)/WhileLoops.cs
+++ 1600Scripting/Assets/SCENES (REDONE STUDY)/Scripts (Redone Practice)/WhileLoops.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WhileLoops : MonoBehaviour {
+public class WhileLoops : MonoBehaviour 
+{
+
+
+	public bool StartGame = true;
 
 	// Use this for initialization
 	void Start () {
-		
+		StartCoroutine( Ticker () );
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	IEnumerator Ticker () 
+	{
+		while (StartGame)
+		{
+			print ("Do Work");
+			yield return new WaitForSeconds (1);
+		}
+
 	}
 }
