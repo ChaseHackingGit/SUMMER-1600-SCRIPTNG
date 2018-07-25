a31508b67593d86540fb770702aa973865f7de35
Chase Hacking <chase.hacking@gmail.com>
2018-07-25 11:28:25 -0600

    Using STARTING PLAYING AND ENDING, We can run our game and use scriptable objects to change the difficulty of playing for the game

---
+++ 1600Scripting/Assets/SCENES (IN CLASS)/Scripts/GameStates/Playing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Playing : GameStatesBase
+{
+	public override void Run()
+	{
+		Debug.Log("Playing");
+	}
+
+
+}
