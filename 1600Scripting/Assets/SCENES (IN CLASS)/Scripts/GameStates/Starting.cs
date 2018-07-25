0d8a4aa8a5a0716bb1738f392d683c4ea86f2f43
Chase Hacking <chase.hacking@gmail.com>
2018-07-25 11:17:14 -0600

    Created more example Scripts, Created a Starting/Playing/Ending Script for the Game. This layout can be used for more than just running the game.

---
+++ 1600Scripting/Assets/SCENES (IN CLASS)/Scripts/GameStates/Starting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Starting : GameStatesBase
+{
+
+	public override void Run()
+	{
+		Debug.Log ("Starting");
+	}
+}
