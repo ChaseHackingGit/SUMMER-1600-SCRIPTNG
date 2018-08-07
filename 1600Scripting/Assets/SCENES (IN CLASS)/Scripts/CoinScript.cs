118d889234dccb053be06df0608702c707cdeabe
Chase Hacking <chase.hacking@gmail.com>
2018-08-07 10:57:05 -0600

    minor tweaks, no changes made.

--- 1600Scripting/Assets/SCENES (IN CLASS)/Scripts/CoinScript.cs
+++ 1600Scripting/Assets/SCENES (IN CLASS)/Scripts/CoinScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour {
 		
+	void Start (){
+	}
+
+	void Update (){
+	}
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Randal") {
