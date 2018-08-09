ed82310d8bd9a15bd3cc7dfeece3e9da4fa37436
Chase Hacking <chase.hacking@gmail.com>
2018-08-08 19:16:46 -0600

    GAME FINALIZED. Added a README script to look at if wanted, and set the layout of the game, when it's opened.

---
+++ 1600Scripting/Assets/READ ME FOLDER/READ ME.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * THE PREMISE OF RANDAL RED(OPTIONAL):
+The game is a top down style game, where the player takes on the roll of a western bandit.
+Your objective as a bandit, is to steal all the loot in the town (represented as purple coins).
+Successfully collecting all the coins will beat the game.
+However, this is easier said than done.
+The townsfolk have held a town meeting, and have all agreed to hunt you down, to stop your awful behavior. 
+These Ai Townsfolk will chase you in an attempt to capture you.
+Getting touched by one of the Townsfolk will have you “caught” and result in a GAME OVER.
+The Townsfolk come in many shapes and sizes (represented as either a pawn, a trickster, or a brute).
+Depending on what the size/color the Townsfolk will be, will decide how fast they can run, so beware. 
+BRUTES are large green Townsfolk, they are slower than the standard yellow PAWNS Townsfolk, but are much larger,
+making them more difficult to avoid. The thin blue colored Townsfolk are the TRICKSTERS,
+they are thin, but much faster than the other two types, so look out. 
+Fortunately, you are a man who has no sympathy for the weak, frail, and unarmed. 
+Naturally as a result of this, you are supplied with a gun.
+You can shoot the Townsfolk to eliminate them. Because there are only so many people in this town, 
+they’ll eventually stop coming, so if you like, you can get out of this alive, 
+with the only consequences being that you will live your live as a mass murderer.
+One drawback to this however, is that you many only shoot facing forward, you cannot shoot in any other direction,
+so there’s certainly still a chance that the Townsfolk will catch you. 
+	It is also possible to jump, which can be used to escape corners, or hide on top of houses like a wuss,
+	keep in mind the Townsfolk will continue to spawn while you camp like a sissy, so when you decide to jump down,
+	be prepared to face the consequences. 
+		One thing to keep in mind, is the county line at the back of the map, 
+		if you step behind this line (represented as a white wall)
+		You will be granted a powerup, that increases the speed of your ammo. 
+			If you can manage to break into the town meeting,
+			you can activate a powder keg, which will eliminate all of the Townsfolk currently outside. 
+
+			Good Luck!
+*/
